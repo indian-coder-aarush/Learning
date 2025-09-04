e1da7ffd4f3392bf124d489ac381da6ca670602a
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-04 23:19:10 +0530

    changing gini impurity function

--- classification_tree.py
+++ classification_tree.py
@@ -1,9 +1,13 @@
 import numpy as np
+import pandas as pd
 
-def gini_impurity(labels):
-    total = labels.sum()
-    gini_impurity = 1
-    for i in range(labels):
-        gini_impurity -= (labels[i] / total)**2
-    return gini_impurity
-
+def gini_impurity(feature,target):
+    classes = []
+    sorted_feature = []
+    probabilities = []
+    for i in range(len(feature)):
+        if not(feature[i] in classes):
+            classes.append(feature[i])
+    for i in range(len(classes)):
+        sorted_feature.append([i for i, value in enumerate(feature) if value == feature[i]])
+        probabilities[i] = len(sorted_feature[i]) / len(feature)
