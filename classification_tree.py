66bd4a40ea38fa1ac2e778519392afdb575063f1
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-11 21:37:15 +0530

    made gini impurity complete

--- classification_tree.py
+++ classification_tree.py
@@ -5,7 +5,12 @@ def gini_impurity(feature,target):
     df = pd.DataFrame({'feature':feature,'target':target})
     df.sort_values(by='feature',inplace = True)
     table = pd.crosstab(df['target'],df['feature'])
-    probability = table.sum()/table.sum().sum()
+    probability_vectors = table/table.sum()
+    calculate_probability_class = probability_vectors.idxmax()
+    probability = ([table.loc[x, y] for x, y in zip(calculate_probability_class.tolist(),
+                                                    calculate_probability_class.index.tolist())]/table.sum())
+    print(table,"\n",probability,"\n",table.loc[calculate_probability_class.tolist(),:].sum())
+    print(table.loc[['A','B'],[1,2]])
     probability.sort_values(inplace = True)
     gini_impurities = {}
     for i in range(len(probability)-1):
@@ -20,7 +25,9 @@ def gini_impurity(feature,target):
         total_impurity = left_impurity*(left_total/(right_total+left_total)) + right_impurity*(right_total/
                                                                                                (right_total+left_total))
         gini_impurities[i+1] = total_impurity
-    least_impurity_key = min(gini_impurities,key=gini_impurities.get)
+    least_impurity_key = min(gini_impurities)
     split = [probability.iloc[least_impurity_key+1:].index.tolist(),
              probability.iloc[:least_impurity_key+1].index.tolist()]
     return gini_impurities[least_impurity_key] , split
+
+print(gini_impurity([1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3],['A','A','A','B','B','B','B','C','C','C','C','C','C','C','C','C','C',]))
